81bdd3d0036ea106231e398d9085f01f0406aea3
Chan Vibol <your_email@gmail.com>
2026-05-16 19:36:17 +0700

    Final actual CORS and Route sync

--- server/index.js
+++ server/index.js
@@ -55,3 +55,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // TRIGGER NEW DEPLOYMENT
+// TRIGGER NEW DEPLOYMENT
+// TRIGGER NEW DEPLOYMENT
